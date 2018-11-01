f45baf8ac95229e39691e6de2b1d80394418aa0f
DKwtno <wei609778992@gmail.com>
2018-11-01 13:30:20 +0800

    管理字体

---
+++ style/font/FontClass.java
@@ -0,0 +1,49 @@
+package style.font;
+
+import javax.swing.UIManager;
+
+public class FontClass {
+	static final int FONT_SIZE = 18;
+	static final String FONT_STYLE = "微软雅黑";
+	public static void loadIndyFont() {
+		UIManager.put("CheckBox.font", new java.awt.Font(FONT_STYLE, 0, FONT_SIZE));
+		UIManager.put("Tree.font", new java.awt.Font(FONT_STYLE, 0, FONT_SIZE));
+		UIManager.put("Viewport.font", new java.awt.Font(FONT_STYLE, 0, FONT_SIZE));
+		UIManager.put("ProgressBar.font", new java.awt.Font(FONT_STYLE, 0, FONT_SIZE));
+		UIManager.put("RadioButtonMenuItem.font", new java.awt.Font(FONT_STYLE, 0, FONT_SIZE));
+		UIManager.put("FormattedTextField.font", new java.awt.Font(FONT_STYLE, 0, FONT_SIZE));
+		UIManager.put("ToolBar.font", new java.awt.Font(FONT_STYLE, 0, FONT_SIZE));
+		UIManager.put("ColorChooser.font", new java.awt.Font(FONT_STYLE, 0, FONT_SIZE));
+		UIManager.put("ToggleButton.font", new java.awt.Font(FONT_STYLE, 0, FONT_SIZE));
+		UIManager.put("Panel.font", new java.awt.Font(FONT_STYLE, 0, FONT_SIZE));
+		UIManager.put("TextArea.font", new java.awt.Font(FONT_STYLE, 0, FONT_SIZE));
+		UIManager.put("Menu.font", new java.awt.Font(FONT_STYLE, 0, FONT_SIZE));
+		UIManager.put("RadioButtonMenuItem.acceleratorFont", new java.awt.Font(FONT_STYLE, 0, FONT_SIZE));
+		UIManager.put("Spinner.font", new java.awt.Font(FONT_STYLE, 0, FONT_SIZE));
+		UIManager.put("Menu.acceleratorFont", new java.awt.Font(FONT_STYLE, 0, FONT_SIZE));
+		UIManager.put("CheckBoxMenuItem.acceleratorFont", new java.awt.Font(FONT_STYLE, 0, FONT_SIZE));
+		UIManager.put("TableHeader.font", new java.awt.Font(FONT_STYLE, 0, FONT_SIZE));
+		UIManager.put("TextField.font", new java.awt.Font(FONT_STYLE, 0, FONT_SIZE));
+		UIManager.put("OptionPane.font", new java.awt.Font(FONT_STYLE, 0, FONT_SIZE));
+		UIManager.put("MenuBar.font", new java.awt.Font(FONT_STYLE, 0, FONT_SIZE));
+		UIManager.put("Button.font", new java.awt.Font(FONT_STYLE, 0, FONT_SIZE));
+		UIManager.put("Label.font", new java.awt.Font(FONT_STYLE, 0, FONT_SIZE));
+		UIManager.put("PasswordField.font", new java.awt.Font(FONT_STYLE, 0, FONT_SIZE));
+		UIManager.put("InternalFrame.titleFont", new java.awt.Font(FONT_STYLE, 0, FONT_SIZE));
+		UIManager.put("OptionPane.buttonFont", new java.awt.Font(FONT_STYLE, 0, FONT_SIZE));
+		UIManager.put("ScrollPane.font", new java.awt.Font(FONT_STYLE, 0, FONT_SIZE));
+		UIManager.put("MenuItem.font", new java.awt.Font(FONT_STYLE, 0, FONT_SIZE));
+		UIManager.put("ToolTip.font", new java.awt.Font(FONT_STYLE, 0, FONT_SIZE));
+		UIManager.put("List.font", new java.awt.Font(FONT_STYLE, 0, FONT_SIZE-2));
+		UIManager.put("OptionPane.messageFont", new java.awt.Font(FONT_STYLE, 0, FONT_SIZE));
+		UIManager.put("EditorPane.font", new java.awt.Font(FONT_STYLE, 0, FONT_SIZE));
+		UIManager.put("Table.font", new java.awt.Font(FONT_STYLE, 0, FONT_SIZE));
+		UIManager.put("TabbedPane.font", new java.awt.Font(FONT_STYLE, 0, FONT_SIZE));
+		UIManager.put("RadioButton.font", new java.awt.Font(FONT_STYLE, 0, FONT_SIZE));
+		UIManager.put("CheckBoxMenuItem.font", new java.awt.Font(FONT_STYLE, 0, FONT_SIZE));
+		UIManager.put("TextPane.font", new java.awt.Font(FONT_STYLE, 0, FONT_SIZE));
+		UIManager.put("PopupMenu.font", new java.awt.Font(FONT_STYLE, 0, FONT_SIZE));
+		UIManager.put("TitledBorder.font", new java.awt.Font(FONT_STYLE, 0, FONT_SIZE));
+		UIManager.put("ComboBox.font", new java.awt.Font(FONT_STYLE, 0, FONT_SIZE));
+	}
+}
